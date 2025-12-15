b398e47c57823947d89ae42936be12f03ba1811e
gajdajviktoria223-ctrl <gajdajviktoria223@gmail.com>
2025-12-15 14:35:09 +0200

    Update main.js

--- what-in-fridge/js/main.js
+++ what-in-fridge/js/main.js
@@ -29,3 +29,22 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 ;
 
+
+document.addEventListener("DOMContentLoaded", () => {
+  // --- Поточний рік (для ©) ---
+  const yearEl = document.getElementById("year");
+  if (yearEl) yearEl.textContent = new Date().getFullYear();
+
+  // --- Поточна дата у футері ---
+  const dateEl = document.getElementById("currentDate");
+  if (dateEl) {
+    const now = new Date();
+    const formatted = now.toLocaleDateString("uk-UA", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+    dateEl.textContent = formatted;
+  }
+});
+
